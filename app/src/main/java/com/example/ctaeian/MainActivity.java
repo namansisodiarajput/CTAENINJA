22f72cbe8354247756ebf51b80d32bc3290afe70
namansisodia <namansisodia619@gmail.com>
2019-06-08 00:14:07 +0530

    changes color and designs

--- app/src/main/java/com/example/ctaeian/MainActivity.java
+++ app/src/main/java/com/example/ctaeian/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ctaeian;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -14,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FirebaseApp.initializeApp(this);
-
 
         Intent intent = new Intent(getApplicationContext(),SideNavigationDrawer.class);
         startActivity(intent);
